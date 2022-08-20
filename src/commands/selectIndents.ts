4e3e111abacc8301b9f2aad9027f81c80d2695d3
Bognaum <bognaum@gmail.com>
2022-08-20 12:34:35 +0300

    Added  selectIndentSpacesBy1, selectIndentSpacesBy2,  selectIndentSpacesBy3, selectIndentSpacesBy4, selectIndentSpacesBy5,  selectIndentTabs, selectIndentSpacesByAuto, selectIndentsAuto.

---
+++ src/commands/selectIndents.ts
@@ -0,0 +1,126 @@
+import * as vsc from "vscode";
+import {
+	searchInIndents,
+	getWholeLinesRange,
+	rangeToOffsets,
+	offsetsToRange,
+	offsetsToSelection,
+	getLineOffsets,
+	separateIndent,
+	getIndentOffset,
+} from "../functions";
+
+export {
+	selectIndentSpacesBy1,
+	selectIndentSpacesBy2,
+	selectIndentSpacesBy3,
+	selectIndentSpacesBy4,
+	selectIndentSpacesBy5,
+	selectIndentTabs,
+	selectIndentSpacesByAuto,
+	selectIndentsAuto
+};
+
+/* export default function selectHarmfulSpaces(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	const 
+		doc  = tEditor.document,
+		opts = tEditor.options,
+		EOL  = ["", "\n", "\r\n"][doc.eol],
+		TAB  = opts.insertSpaces && typeof opts.tabSize === "number" ? 
+		" ".repeat(opts.tabSize) : "\t",
+		newSelections: vsc.Selection[] = [];
+	
+	tEditor.edit((edit) => {
+		for (let sel of tEditor.selections) {
+			const 
+				wholeStringsRange = getWholeLinesRange(doc, sel),
+				startOffset = doc.offsetAt(wholeStringsRange.start),
+				text = doc.getText(wholeStringsRange),
+				harmfulSpaces: [number, number][] = searchInIndents(
+					text, 
+					EOL, 
+					/ (?=\t)/g, 
+					startOffset
+				);
+
+			for (const offs of harmfulSpaces) {
+				newSelections.push(offsetsToSelection(doc, offs));
+			}
+			// edit.replace(sel, doc.getText(sel).trim());
+			// newSelections.push(sel);
+		}
+		tEditor.selections = newSelections;
+	});
+} */
+
+function selectIndentSpacesBy1(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, / /g);
+}
+
+function selectIndentSpacesBy2(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, /  /g);
+}
+
+function selectIndentSpacesBy3(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, /   /g);
+}
+
+function selectIndentSpacesBy4(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, /    /g);
+}
+
+function selectIndentSpacesBy5(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, /     /g);
+}
+
+function selectIndentTabs(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	selectBy(tEditor, edit, /\t/g);
+}
+
+function selectIndentSpacesByAuto(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	const 
+		opts = tEditor.options,
+		TAB  = opts.insertSpaces && typeof opts.tabSize === "number" ? 
+			" ".repeat(opts.tabSize) : "\t",
+		size:number = opts.tabSize as number;
+	selectBy(tEditor, edit, new RegExp(` {${size}}`, "g"));
+}
+
+function selectIndentsAuto(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+	const 
+		opts = tEditor.options,
+		TAB  = opts.insertSpaces && typeof opts.tabSize === "number" ? 
+			" ".repeat(opts.tabSize) : "\t";
+	selectBy(tEditor, edit, new RegExp(TAB, "g"));
+}
+
+function selectBy(
+	tEditor: vsc.TextEditor, 
+	edit: vsc.TextEditorEdit, 
+	re: RegExp
+) {
+	const 
+		doc  = tEditor.document,
+		EOL  = ["", "\n", "\r\n"][doc.eol],
+		newSelections: vsc.Selection[] = [];
+	
+	tEditor.edit((edit) => {
+		for (let sel of tEditor.selections) {
+			const 
+				wholeStringsRange = getWholeLinesRange(doc, sel),
+				startOffset = doc.offsetAt(wholeStringsRange.start),
+				text = doc.getText(wholeStringsRange),
+				harmfulSpaces: [number, number][] = searchInIndents(
+					text, 
+					EOL, 
+					re, 
+					startOffset
+				);
+
+			for (const offs of harmfulSpaces) {
+				newSelections.push(offsetsToSelection(doc, offs));
+			}
+		}
+		tEditor.selections = newSelections;
+	});
+}
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,17 @@ import alignIndents from './commands/alignIndents';
 import alignIndentSteps from './commands/alignIndentSteps';
 import selectHarmfulSpaces from './commands/selectHarmfulSpaces';
 
+import { 
+	selectIndentSpacesBy1,
+	selectIndentSpacesBy2,
+	selectIndentSpacesBy3,
+	selectIndentSpacesBy4,
+	selectIndentSpacesBy5,
+	selectIndentTabs,
+	selectIndentSpacesByAuto,
+	selectIndentsAuto
+ } from "./commands/selectIndents";
+
 export function activate(context: vsc.ExtensionContext) {
 	const commands = [
 		// vsc.commands.registerCommand('my-command', () => {}),
@@ -10,6 +21,15 @@ export function activate(context: vsc.ExtensionContext) {
 		vsc.commands.registerTextEditorCommand('align-indents.alignIndents', alignIndents),
 		vsc.commands.registerTextEditorCommand('align-indents.alignIndentSteps', alignIndentSteps),
 		vsc.commands.registerTextEditorCommand('align-indents.selectHarmfulSpaces', selectHarmfulSpaces),
+
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesBy1',    selectIndentSpacesBy1   ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesBy2',    selectIndentSpacesBy2   ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesBy3',    selectIndentSpacesBy3   ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesBy4',    selectIndentSpacesBy4   ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesBy5',    selectIndentSpacesBy5   ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentTabs',         selectIndentTabs        ),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentSpacesByAuto', selectIndentSpacesByAuto),
+		vsc.commands.registerTextEditorCommand('align-indents.selectIndentsAuto',        selectIndentsAuto       ),
 	];
 
 	context.subscriptions.push(...commands);
