9860aa03862a662a3246b542bb620e26da9e675b
Bognaum <bognaum@gmail.com>
2022-08-20 12:59:41 +0300

    ---

--- src/commands/selectIndents.ts
+++ src/commands/selectIndents.ts
@@ -21,38 +21,6 @@ export {
 	selectIndentsAuto
 };
 
-/* export default function selectHarmfulSpaces(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
-	const 
-		doc  = tEditor.document,
-		opts = tEditor.options,
-		EOL  = ["", "\n", "\r\n"][doc.eol],
-		TAB  = opts.insertSpaces && typeof opts.tabSize === "number" ? 
-		" ".repeat(opts.tabSize) : "\t",
-		newSelections: vsc.Selection[] = [];
-	
-	tEditor.edit((edit) => {
-		for (let sel of tEditor.selections) {
-			const 
-				wholeStringsRange = getWholeLinesRange(doc, sel),
-				startOffset = doc.offsetAt(wholeStringsRange.start),
-				text = doc.getText(wholeStringsRange),
-				harmfulSpaces: [number, number][] = searchInIndents(
-					text, 
-					EOL, 
-					/ (?=\t)/g, 
-					startOffset
-				);
-
-			for (const offs of harmfulSpaces) {
-				newSelections.push(offsetsToSelection(doc, offs));
-			}
-			// edit.replace(sel, doc.getText(sel).trim());
-			// newSelections.push(sel);
-		}
-		tEditor.selections = newSelections;
-	});
-} */
-
 function selectIndentSpacesBy1(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
 	selectBy(tEditor, edit, / /g);
 }
